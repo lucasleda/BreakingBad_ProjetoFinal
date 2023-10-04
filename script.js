7b393eeff8a7ce1008a5696c8e4eeb2caaa9a90b
gabriel oliveira <gabrielogbusiness@gmail.com>
2023-10-03 22:19:39 -0300

    adicionado funcionalidade na página cortes: 'adicione seu vídeo', com js.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let b1 = document.getElementById("b1")
+/*let b1 = document.getElementById("b1")
 b1.onclick = () => {
     alert("Esta função não está disponível.")
-} 
+}*/
+
+let buttonInputVideo = document.getElementById("buttonInputVideo")
+buttonInputVideo.onclick = () => {
+    alert("Funcionalidade em desenvolvimento")
+}
