b9c3dadfae174b48c71450eb9d4515711a4bf9d3
Pedro Martins <pedrohenrique.251@hotmail.com>
2023-10-10 17:28:31 -0300

    Remoção container de newsletter

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*let b1 = document.getElementById("b1")
-b1.onclick = () => {
-    alert("Esta função não está disponível.")
-}*/
+
 
 let buttonInputVideo = document.getElementById("buttonInputVideo")
 buttonInputVideo.onclick = () => {
